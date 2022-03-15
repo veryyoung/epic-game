8d45031d164878d908a01bb7a3355f51d8905b84
veryyoung <veryyoung.me@gmail.com>
2022-03-15 17:58:50 +0800

    💎 Actually mint your NFTs locally.

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,21 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Leo", "Aang", "Pikachu"],       // Names
+    ["https://i.imgur.com/pKd5Sdk.png", // Images
+    "https://i.imgur.com/xVu4vFL.png", 
+    "https://i.imgur.com/WMB6g9u.png"],
+    [100, 200, 300],                    // HP values
+    [100, 50, 25]                       // Attack damage values
+  );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
+
+  let txn = await gameContract.mintCharacterNFT(2);
+  await txn.wait();
+
+  let returnTokenUri = await gameContract.tokenURI(1);
+  console.log("Token URI:", returnTokenUri);
 }
 
 const runMain = async () => {
